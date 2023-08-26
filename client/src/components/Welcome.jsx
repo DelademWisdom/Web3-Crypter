7b1a3a6a423e1311e6cc4253ef9947aec332390a
Deladem <wisdomdelademkugbe@gmail.com>
2023-08-26 19:53:17 +0000

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -89,6 +89,10 @@ const Welcome = () => {
 
             <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
                 <input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+                <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+
             </div>            
 
         </div>
